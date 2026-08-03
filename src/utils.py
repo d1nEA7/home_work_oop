73cb7c8434739d5698c188764c3d5632ca34e4e9
Denis Galkin <dis43@mail.ru>
2026-08-03 20:35:39 +0300

    add new_func

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,5 @@
+
+
 class Product:
     """Класс товары"""
     name: str  # Название товара (строка)
@@ -8,9 +10,28 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
+
+    @classmethod
+    def new_product(cls, data: dict) -> Product:
+        name = data.get("name")  # "Samsung Galaxy S23 Ultra"
+        description = data.get("description")
+        price = data.get("price")
+        quantity = data.get("quantity")
+        return Product(name, description, price, quantity)
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        if value <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = value
 
 
 
